bfe043363ba320aeea2dba197aae07e60322da74
michaelfarias <michaelfarias01@gmail.com>
2021-07-12 16:09:24 -0300

    Ajustes no endpoint /categorias/{id}

--- cursospring/src/main/java/com/udemy/cursospring/services/CategoriaService.java
+++ cursospring/src/main/java/com/udemy/cursospring/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.udemy.cursospring.domain.Categoria;
 import com.udemy.cursospring.repositories.CategoriaRepository;
+import com.udemy.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 
-		return obj.orElse(null);
+		return obj.orElseThrow((() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id:" + id + ", Tipo: " + Categoria.class.getName())));
 	}
 }
