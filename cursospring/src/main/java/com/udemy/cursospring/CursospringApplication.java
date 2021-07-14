1a0dff413bf08c1329fbbcc533c8ed63e36b6ad5
michaelfarias <michaelfarias01@gmail.com>
2021-07-14 16:45:08 -0300

    Pedido, EstadoPagamento e Pagamento

--- cursospring/src/main/java/com/udemy/cursospring/CursospringApplication.java
+++ cursospring/src/main/java/com/udemy/cursospring/CursospringApplication.java
@@ -1,5 +1,6 @@
 package com.udemy.cursospring;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.udemy.cursospring.domain.Cidade;
 import com.udemy.cursospring.domain.Cliente;
 import com.udemy.cursospring.domain.Endereco;
 import com.udemy.cursospring.domain.Estado;
+import com.udemy.cursospring.domain.Pagamento;
+import com.udemy.cursospring.domain.PagamentoComBoleto;
+import com.udemy.cursospring.domain.PagamentoComCartao;
+import com.udemy.cursospring.domain.Pedido;
 import com.udemy.cursospring.domain.Produto;
+import com.udemy.cursospring.domain.enums.EstadoPagamento;
 import com.udemy.cursospring.domain.enums.TipoCliente;
 import com.udemy.cursospring.repositories.CategoriaRepository;
 import com.udemy.cursospring.repositories.CidadeRepository;
 import com.udemy.cursospring.repositories.ClienteRepository;
 import com.udemy.cursospring.repositories.EnderecoRepository;
 import com.udemy.cursospring.repositories.EstadoRepository;
+import com.udemy.cursospring.repositories.PagamentoRepository;
+import com.udemy.cursospring.repositories.PedidoRepository;
 import com.udemy.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursospringApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -86,6 +98,22 @@ public class CursospringApplication implements CommandLineRunner {
 		clienteRepository.save(cli1);
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2021 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2021 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2021 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
---
+++ cursospring/src/main/java/com/udemy/cursospring/domain/PagamentoComBoleto.java
@@ -0,0 +1,46 @@
+package com.udemy.cursospring.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.udemy.cursospring.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+
+	private static final long serialVersionUID = 1L;
+
+	private Date dataVecimento;
+	private Date dataPagamento;
+
+	public PagamentoComBoleto() {
+
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamento) {
+
+		super(id, estado, pedido);
+
+		this.dataPagamento = dataPagamento;
+		this.dataVecimento = dataVencimento;
+	}
+
+	public Date getDataVecimento() {
+		return dataVecimento;
+	}
+
+	public void setDataVecimento(Date dataVecimento) {
+		this.dataVecimento = dataVecimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+}
