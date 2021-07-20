682722bb939b0c2905d19883e2d83b34ddebdb5b
michaelfarias <michaelfarias01@gmail.com>
2021-07-20 11:36:56 -0300

    validação customizada: email não repetido na inserção de cliente

--- cursospring/src/main/java/com/udemy/cursospring/repositories/ClienteRepository.java
+++ cursospring/src/main/java/com/udemy/cursospring/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.udemy.cursospring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.udemy.cursospring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
