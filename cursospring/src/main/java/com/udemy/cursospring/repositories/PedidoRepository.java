8fca4ce79f369feae4013a7791d837ddfd131d97
michaelfarias <michaelfarias01@gmail.com>
2021-07-23 17:01:38 -0300

    Restrição de conteúdo: cliente so recupera seus pedidos

--- cursospring/src/main/java/com/udemy/cursospring/repositories/PedidoRepository.java
+++ cursospring/src/main/java/com/udemy/cursospring/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.udemy.cursospring.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.udemy.cursospring.domain.Cliente;
 import com.udemy.cursospring.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- cursospring/src/main/java/com/udemy/cursospring/services/PedidoService.java
+++ cursospring/src/main/java/com/udemy/cursospring/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.udemy.cursospring.domain.Cliente;
 import com.udemy.cursospring.domain.ItemPedido;
 import com.udemy.cursospring.domain.PagamentoComBoleto;
 import com.udemy.cursospring.domain.Pedido;
@@ -15,6 +19,8 @@ import com.udemy.cursospring.domain.enums.EstadoPagamento;
 import com.udemy.cursospring.repositories.ItemPedidoRepository;
 import com.udemy.cursospring.repositories.PagamentoRepository;
 import com.udemy.cursospring.repositories.PedidoRepository;
+import com.udemy.cursospring.secutiry.UserSS;
+import com.udemy.cursospring.services.exceptions.AuthorizationException;
 import com.udemy.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -72,5 +78,16 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.autheticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
