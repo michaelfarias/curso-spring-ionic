5b4ed134dbb761f68ec1b15b3b26d47504806f07
michaelfarias <michaelfarias01@gmail.com>
2021-07-26 16:01:29 -0300

    Salvando primeiro arquivo no S3

--- cursospring/src/main/java/com/udemy/cursospring/CursospringApplication.java
+++ cursospring/src/main/java/com/udemy/cursospring/CursospringApplication.java
@@ -1,11 +1,17 @@
 package com.udemy.cursospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.udemy.cursospring.services.S3Service;
+
 @SpringBootApplication
 public class CursospringApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -13,7 +19,7 @@ public class CursospringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("/home/michael/Imagens/HONDA_08_Twister_8669-1024x682.jpg");
 	}
 
 }
