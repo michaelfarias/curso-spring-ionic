6d5f63295aeff49198d9235f3154eb6d23bea7eb
michaelfarias <michaelfarias01@gmail.com>
2021-07-27 09:34:52 -0300

    Enviando imagem via endpoint

--- cursospring/src/main/java/com/udemy/cursospring/services/ClienteService.java
+++ cursospring/src/main/java/com/udemy/cursospring/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.udemy.cursospring.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.udemy.cursospring.domain.Cidade;
 import com.udemy.cursospring.domain.Cliente;
@@ -33,8 +35,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 
@@ -113,5 +119,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
